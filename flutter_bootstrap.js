582da5e4d336c5dc3627f6103e453af969be3414
sawantsanket855 <sawantsanket855@gmail.com>
2025-07-11 13:08:14 +0530

    pdf preview

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -40,6 +40,6 @@ _flutter.buildConfig = {"engineRevision":"50f226569f331edf2831a7543f55d953c3d506
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3806531458"
+    serviceWorkerVersion: "3192825216"
   }
 });
